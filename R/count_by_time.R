58c41099941e49db00576413c8c04a213e7032a2
runzw <runzw@students.cs.ubc.ca>
2022-11-15 19:37:14 -0800

    hello

---
+++ R/count_by_time.R
@@ -0,0 +1,48 @@
+#' @title count_by_time function
+#'
+#' @details Given a Date column, count the occurrence of a date unit specified by floor_unit. This could be useful when finding the pattern between values and specified time frames.
+#'
+#' @param dataset, the dataset to be analyzed
+#'
+#' @param date_column, name of the Date column on which to be applied the operation
+#'
+#' @param floor_unit, the date unit to be counted
+#'
+#' @return a N X 2 dataframe with column "time" and "count", which represents the list of date unit values and the number of their occurrence, respectively.
+#'
+#' @examples
+#' library(lubridate)
+#' library(dplyr)
+#' library(datateachr)
+#' library(tidyr)
+#' count_by_time(datateachr::vausencouver_trees, "date_planted", "year")
+#' count_by_time(datateachr::vancouver_trees, "date_planted", "month")
+#' count_by_time(datateachr::vancouver_trees, "date_planted", "day")
+#'
+#' @export
+#'
+count_by_time <- function(dataset, date_column, floor_unit) {
+  if (!is.character(date_column)) {
+    stop("The selected column must be a string.")
+  }
+
+  if(!(date_column %in% names(dataset))){
+    stop("The selected column does not exist in the given dataset.")
+  }
+
+  if(!(inherits(dataset[[date_column]], 'Date'))){
+    stop("The selected column must be a Date.")
+  }
+
+  if(!(floor_unit %in% c("year", "month", "day"))){
+    stop("floor_unit must be one of year, month, or day.")
+  }
+
+  format_unit <- if(floor_unit == "year") "%Y" else (if(floor_unit == "month") "%b" else "%d")
+  return (dataset %>%
+            tidyr::drop_na(.data[[date_column]]) %>%
+            dplyr::mutate(date_floored = lubridate::floor_date(.data[[date_column]], unit=floor_unit)) %>%
+            dplyr::mutate(time = format(date_floored, format_unit)) %>%
+            dplyr::group_by(time) %>%
+            dplyr::count(time, name = "count"))
+}
---
+++ tests/testthat/test-count_by_time.R
@@ -0,0 +1,37 @@
+count_by_year <- vancouver_trees %>%
+  drop_na(date_planted) %>%
+  mutate(date_floored = floor_date(date_planted, unit="year")) %>%
+  mutate(time = format(date_floored, "%Y")) %>%
+  group_by(time) %>%
+  count(time, name = "count")
+
+count_by_month <- vancouver_trees %>%
+  drop_na(date_planted) %>%
+  mutate(date_floored = floor_date(date_planted, unit="month")) %>%
+  mutate(time = format(date_floored, "%b")) %>%
+  group_by(time) %>%
+  count(time, name = "count")
+
+count_by_day <- vancouver_trees %>%
+  drop_na(date_planted) %>%
+  mutate(date_floored = floor_date(date_planted, unit="day")) %>%
+  mutate(time = format(date_floored, "%d")) %>%
+  group_by(time) %>%
+  count(time, name = "count")
+
+test_that("Test count_by_time function", {
+  # Test if it handles "year" correctly
+  expect_equal(count_by_time(vancouver_trees, "date_planted", "year"), count_by_year)
+  # Test if it handles "month" correctly
+  expect_equal(count_by_time(vancouver_trees, "date_planted", "month"), count_by_month)
+  # Test if it handles "day" correctly
+  expect_equal(count_by_time(vancouver_trees, "date_planted", "day"), count_by_day)
+  # Test if it throws an error when the input column is an object
+  expect_error(count_by_time(vancouver_trees, date_planted, "year"), 'The selected column must be a string.')
+  # Test if it throws an error when the input column does not exist
+  expect_error(count_by_time(vancouver_trees, "planted_date", "year"), 'The selected column does not exist in the given dataset.')
+  # Test if it throws an error when the input column is not a Date column
+  expect_error(count_by_time(vancouver_trees, "curb", "year"), 'The selected column must be a Date.')
+  # Test if it throws an error when the time unit is not valid option
+  expect_error(count_by_time(vancouver_trees, "date_planted", "hour"), 'floor_unit must be one of year, month, or day.')
+})
